1f5c53f9c265c3f274c9a5a6072543fe739ab0cf
Tejas Rangole <rangole@royal-17.cs.wisc.edu>
2021-02-24 16:29:50 -0600

    updated Switch.java

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,21 +1,22 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.MACAddress;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 import java.util.Map;
 import java.util.HashMap;
+import net.floodlightcontroller.packet.MACAddress;
+import java.lang.Thread;
+import edu.wisc.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
-public class Switch extends Device implements Runnable
-{	
-	// switch table mapping mac address to interface
-	Map<MacAddress, Iface> switchTable = new HashMap<>();
+public class Switch extends Device
+{	// switch table mapping mac address to interface
+	Map<MACAddress, Iface> switchTable = new HashMap<>();
 	// mac address to start time mapping, used to check if switch table entry is valid
-	Map<MACAddress, long> ttlMap = new HashMap<>();
+	Map<MACAddress, Long> ttlMap = new HashMap<>();
 	static long TIMEOUT = 15000; // time for mac address to live in ms
 	/**
 	 * Creates a router for a specific host.
@@ -33,23 +34,17 @@ public class Switch extends Device implements Runnable
 	 */
 	public void handlePacket(Ethernet etherPacket, Iface inIface)
 	{
-		System.out.println("*** -> Received packet: " +
-				etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-
-		MACAddress source = etherPacket.getSourceMAC();
+        MACAddress source = etherPacket.getSourceMAC();
 		MACAddress dest = etherPacket.getDestinationMAC();
-		// put in new entry for source mac address
         switchTable.put(source, inIface);
-		Runnable runnable = new Runnable(){
+		/*
+        Runnable runnable = new Runnable(){
 			public void run(){
 				try {
 					while(true){
 						Thread.sleep(1000); // run every 1 second
 						// searches through ttlMap and discards expired entries in the switch table
-                        for(Map.Entry<MacAddress, long> entry : ttlMap.entrySet()){
+                        for(Map.Entry<MacAddress, Long> entry : ttlMap.entrySet()){
 							long currentTime = System.currentTimeMillis();
 							if(currentTime - entry.getValue() > TIMEOUT){
 								switchTable.remove(entry.getKey());
@@ -62,11 +57,16 @@ public class Switch extends Device implements Runnable
 				}
 			}
 		}
+		
 		// thread to check expired entries in switch table
 		Thread updater = new Thread(runnable);
         updater.start();
-		Iface toInterface = null;
+        */
+
+        Iface toInterface = null;
+		
 		Map<String,Iface> interfaces = getInterfaces();
+		
         // line 41 - 47 : gets the interface to send the packet out of
 		for (Object name : interfaces.keySet()){
 			Iface interface = getInterface(name);
@@ -76,22 +76,12 @@ public class Switch extends Device implements Runnable
 			}
 		}
         
-		if (toInterface != null){ // destination interface found
-			if (!sendPacket(etherPacket, interface)) System.out.println("Packet was not sent successfully");
-			return; 
-		}
-        // floods all devices by sending out on all interfaces
-		for (Object name : interfaces.keySet()){
-			interface = getInterface(name);
-			// need semicolon?
-			sendPacket(etherPacket, interface);
-		}
+		System.out.println("*** -> Received packet: " +
+				etherPacket.toString().replace("\n", "\n\t"));
+		
+		/********************************************************************/
+		/* TODO: Handle packets                                             */
 		
 		/********************************************************************/
 	}
-
-	private void checkPacket(){ // will have an infinite loop that has an iteration inside a synchronized black
-
-	}
-	
-
+}
