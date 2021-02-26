b34ccc94225d967646f95efae5d361fd93d5dbd2
TejasRangole1 <rangole@wisc.edu>
2021-02-25 17:09:46 -0800

    Working Switch.java with table deletes

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -5,8 +5,11 @@ import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.HashMap;
+import java.util.Iterator;
+
 import net.floodlightcontroller.packet.MACAddress;
 import java.lang.Thread;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -21,6 +24,43 @@ public class Switch extends Device {
 	// valid
 	Map<MACAddress, Long> ttlMap = new HashMap<>();
 	static long TIMEOUT = 15000; // time for mac address to live in ms
+    Thread thread;
+	ReentrantLock lock = new ReentrantLock(); // lock to control access to switch table and ttl map
+
+	private class SwitchRunnable implements Runnable {
+		
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			// removed expired entries from switch table and then sleep for 1 second and
+			// repeat the process
+			while (true) {
+				try {
+					lock.lock();
+					Iterator<Entry<MACAddress, Long>> it = ttlMap.entrySet().iterator();
+					while(it.hasNext()) {
+						Map.Entry<MACAddress, Long> entry = it.next();
+						if( System.currentTimeMillis() - entry.getValue() > TIMEOUT) {
+							switchTable.remove(entry.getKey());
+							System.out.println("removed MACAddress : " + entry.getKey().toString());
+							it.remove();
+						}
+					}
+					
+				} finally {
+					lock.unlock();
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
 	/**
 	 * Creates a router for a specific host.
 	 * 
@@ -28,30 +68,9 @@ public class Switch extends Device {
 	 */
 	public Switch(String host, DumpFile logfile) {
 		super(host, logfile);
-		
-		Runnable runnable = new Runnable() {
-			public void run() {
-				while (true) {
-					synchronized (Switch.this) {
-						System.out.println("finding expired entries");
-						for (Map.Entry<MACAddress, Long> entry : ttlMap.entrySet()) {
-							if (System.currentTimeMillis() - entry.getValue() > TIMEOUT) {
-								switchTable.remove(entry.getKey());
-								ttlMap.remove(entry.getKey());
-							}
-						}
-					}
-					System.out.println("searched ttlMap");
-					try {
-						System.out.println("sleeping");
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
-		
+		Runnable runnable = new SwitchRunnable();
+        thread = new Thread(runnable);
+        thread.start();
 	}
 
 	/**
@@ -69,22 +88,24 @@ public class Switch extends Device {
 		MACAddress source = etherPacket.getSourceMAC();
 		MACAddress dest = etherPacket.getDestinationMAC();
 		// adds/updates switch table upon revieval of packet
-		synchronized (Switch.this) {
+		try {
+			lock.lock();
 			switchTable.put(source, inIface);
 			ttlMap.put(source, System.currentTimeMillis());
+		} finally {
+			lock.unlock();
 		}
 		Map<String, Iface> interfaces = getInterfaces();
-		if(switchTable.containsKey(dest)) {
+		if (switchTable.containsKey(dest)) {
 			sendPacket(etherPacket, switchTable.get(dest));
-		}
-		else {
-			for(Map.Entry<String, Iface> entry : interfaces.entrySet()) {
-				if(!entry.getValue().getName().equals(inIface.getName())) {
+		} else {
+			for (Map.Entry<String, Iface> entry : interfaces.entrySet()) {
+				if (!entry.getValue().getName().equals(inIface.getName())) {
 					sendPacket(etherPacket, entry.getValue());
 				}
 			}
 		}
-		
+
 	}
 
 }
